133fc9d205108c1a10f35f18c444617b2c516801
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-12-21 10:24:22 -0300

    feat: importing remote component on host

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Button from "remoteApp/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Host Application</h1>
+      <Button />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
